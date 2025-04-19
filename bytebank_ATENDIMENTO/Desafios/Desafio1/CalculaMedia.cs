5b36b2e76b7aa0a7500ba1d178003a2f9d67939c
VickyPrado <vicky.prado05@gmail.com>
2025-04-19 18:53:50 -0300

    deasfio1

---
+++ bytebank_ATENDIMENTO/Desafios/Desafio1/CalculaMedia.cs
@@ -0,0 +1,24 @@
+﻿/*
+using System;
+
+double MediaDaAmostra(double[] amostra)
+{
+    double media = 0;
+    double acumulador = 0;
+        
+    if (amostra == null || amostra.Length == 0)
+    {
+        Console.WriteLine("Amostra de dados está vazio ou nulo.");
+        return 0;
+    }
+    else
+    {
+        for (int i = 0; i < amostra.Length; i++)
+        {
+            acumulador += amostra[i];
+        }
+        media = acumulador / amostra.Length;
+    }
+    return media;
+}
+*/
