63b9e78613afe9f612f65f102cc3857b3030cc0f
VickyPrado <vicky.prado05@gmail.com>
2025-04-19 14:33:42 -0300

    criando-array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,3 +1,28 @@
 ﻿using System;
 
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+TestaArrayInt();
+
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"O tamanho do array é: {idades.Length}");
+
+    int acumulador = 0;
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Índice [{i}] = {idade}");
+        acumulador += idade;
+    }
+
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"A média de idades é: {media}");
+}
