19361b52c0e2c8cc2f04f87352e9749bf6239c29
VickyPrado <vicky.prado05@gmail.com>
2025-05-04 18:42:59 -0300

    desafio3

---
+++ bytebank_ATENDIMENTO/Desafios/Desafio3/VerificaNome.cs
@@ -0,0 +1,33 @@
+﻿//3. Verificar se existe um nome específico na lista, por exemplo “Anakin Wayne”. 
+
+/*
+List<string> nomesDosEscolhidos = new List<string>()
+{
+    "Bruce Wayne",
+    "Carlos Vilagran",
+    "Richard Grayson",
+    "Bob Kane",
+    "Will Farrel",
+    "Lois Lane",
+    "General Welling",
+    "Perla Letícia",
+    "Uxas",
+    "Diana Prince",
+    "Elisabeth Romanova",
+    "Anakin Wayne"
+};
+
+bool VerificaNomes(List<string> nomesDosEscolhidos, string escolhido)
+{
+    return nomesDosEscolhidos.Contains(escolhido);
+}
+
+if (nomesDosEscolhidos.Contains("Anakin Wayne"))
+{
+    Console.WriteLine($"o nome foi encontrado na lista");
+}
+else
+{
+    Console.WriteLine("o nome não foi encontrado na lista");
+}
+*/
